c7778aedb39e8bc58068fa211ee7b7b23372549c
kennethwty <kennethwty93@gmail.com>
2018-09-01 16:38:28 -0400

    Upload Day 10: Binary Numbers

--- src/Day10.java
+++ src/Day10.java
@@ -9,16 +9,16 @@ import java.util.Scanner;
         Given a base-10 integer, n, convert it to binary (base-2).
         Then find and print the base-10 integer denoting the maximum number of consecutive 1's in n's binary representation.
 
-        Sample Input 1
+        Sample Input 1:
             5
 
-        Sample Output 1
+        Sample Output 1:
             1
 
-        Sample Input 2
+        Sample Input 2:
             13
 
-        Sample Output 2
+        Sample Output 2:
             2
 
         Explanation:
---
+++ src/Day11.java
@@ -0,0 +1,14 @@
+package src;
+
+/*
+    By: Tszyan "Kenneth" Wong
+
+    Sample Input:
+        Welcome to 30 Days of Code!
+
+    Sample Output:
+        Hello, World.
+        Welcome to 30 Days of Code!
+*/
+public class Day11 {
+}
