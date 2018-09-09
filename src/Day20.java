bc21462cc215b989215cc3cb8bccf6a197c7b2be
kennethwty <kennethwty93@gmail.com>
2018-09-09 10:59:13 -0400

    Upload Day 20: Sorting

--- src/Day20.java
+++ src/Day20.java
@@ -39,8 +39,8 @@ public class Day20 {
     public static void bubbleSort(int[] arr) {
         int numOfSwaps = 0;
 
-        for(int i = 0; i < arr.length; i++) {
-            for(int j = 0; j < arr.length - 1; j++) {
+        for(int last = arr.length - 1; last > 0; last--) {
+            for(int j = 0; j < last; j++) {
                 if(arr[j] > arr[j+1]) {
                     swap(arr, j, j+1);
                     numOfSwaps++;
